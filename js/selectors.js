1f92535dbc33061ce979342eb1dc1234c14f6255
Lisa Chan <xlisachan@gmail.com>
2018-02-21 15:18:07 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -18,5 +18,3 @@ function divSelector() {
 function firstListItem() {
   return $('pics ul:first-child');
 }
-
-he function should use a first-child selector to return the first list item in the ul with the ID pic-list.
