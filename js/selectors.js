552841c80aa3117532d5cfa6df43a6ec30979662
Lisa Chan <xlisachan@gmail.com>
2018-02-21 15:14:44 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,5 +4,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  return $('div img: last-child');
+  return $('use the last jQuery selector to return the last image in ');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.#pics');
+}
+
+function firstListItem() {
+  return $('pics pic-list: first-child');
 }
